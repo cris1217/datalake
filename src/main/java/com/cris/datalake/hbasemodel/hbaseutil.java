e0f20480c1bece3b210498ba7d20c00cec1478e8
Poojitha1 <ksainath.dba@gmail.com>
2017-11-25 01:13:30 +0530

    Starting Lake Project

---
+++ src/main/java/com/cris/datalake/hbasemodel/hbaseutil.java
@@ -0,0 +1,34 @@
+package com.cris.datalake.hbasemodel;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.conf.Configuration;
+public class hbaseutil {
+    public static void main(String[] args) {
+        // Instantiating configuration class
+        Configuration con = HBaseConfiguration.create();
+
+        // Instantiating HbaseAdmin class
+        HBaseAdmin admin = new HBaseAdmin(con);
+
+        // Instantiating table descriptor class
+        HTableDescriptor tableDescriptor = new
+                HTableDescriptor(TableName.valueOf("emp"));
+
+        // Adding column families to table descriptor
+        tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+        tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+
+        // Execute the table through admin
+        admin.createTable(tableDescriptor);
+        System.out.println(" Table created ");
+
+
+    }
+}
+
